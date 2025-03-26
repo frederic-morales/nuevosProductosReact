545238d8f6854d8cc2f48ca2ac6bfa9bc584f53b
Frederic Soc <desarrollador2@wellcopharma.com>
2025-03-26 15:15:11 -0600

    Agregando la informacion correspondiente por cada etapa

---
+++ src/hooks/fetch_etapa_historial.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const fetch_etapa_historial = ({ desarrolloProductoId, etapaId }) => {
+  const [etapaHistorial, setEtapaHistorial] = useState([]) // Campos traídos desde la DB
+  const [loadingHistorial, setloadingHistorial] = useState(true) // Estado de carga
+  const [errorHistorial, setErrorHistorial] = useState(null) // Manejo de errores
+
+  const api = import.meta.env.VITE_API_URL
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const historialResponse = await axios.get(
+          `${api}/etapas/${desarrolloProductoId}/historial/${etapaId}`
+        )
+        setEtapaHistorial(await historialResponse.data)
+      } catch (err) {
+        setloadingHistorial(
+          err instanceof Error ? err.message : 'Ocurrió un error'
+        )
+      } finally {
+        setErrorHistorial(false)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  return { etapaHistorial, loadingHistorial, errorHistorial }
+}
+
+export default fetch_etapa_historial
---
+++ src/hooks/post_etapa_actualizar.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+const post_etapa_actualizar = async ({
+  ProgresoEtapaId,
+  Estado,
+  RutaDoc,
+  Descripcion
+}) => {
+  try {
+    const API = import.meta.env.VITE_API_URL
+    const response = await axios.post(`${API}/etapa/progreso/actualizacion`, {
+      ProgresoEtapaId: ProgresoEtapaId,
+      Estado: Estado,
+      RutaDoc: RutaDoc,
+      Descripcion: Descripcion
+    })
+
+    console.log(await response.data)
+    return await response
+  } catch (err) {
+    console.log('Error al iniciar la etapa...', err)
+    return null
+  }
+}
+
+export default post_etapa_actualizar
